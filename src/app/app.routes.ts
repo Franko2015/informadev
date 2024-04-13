aaca1d87bbc9d22362abf489ad5fb81f506e0497
Franko20155 <franko20155@gmail.com>
2024-04-13 07:03:23 -0400

    Primer commit con las rutas hechas y direccionadas en caso de error del sistema.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { AccountComponent } from './home/account/account.component';
+import { AdminComponent } from './home/admin/admin.component';
+import { PageComponent } from './home/page/page.component';
+import { SettingsComponent } from './home/settings/settings.component';
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login/:', component: LoginComponent},
+  {path: '**', component: ErrorComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'account', children: [
+    {path: 'admin', component: AdminComponent},
+    {path: 'page', component: PageComponent},
+    {path: 'settings', component: SettingsComponent}
+  ]}
+];
